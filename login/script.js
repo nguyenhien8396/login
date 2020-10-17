4af8f6fc19406c95af563033cf8fe9990eb03656
Hiền Nguyễn <nguyenhien8396@gmail.com>
2020-10-17 21:28:28 +0700

    login

--- login/script.js
+++ login/script.js
@@ -11,4 +11,11 @@ $(document).ready(function () {
     var input = $(".log-status input");
     input.attr("type", input.get(0).type === "text" ? "password" : "text");
   });
+  $(".log-status .form-control").keyup(function () {
+    if ($(".log-status .form-control").val() !== "") {
+      $(".see-password").css("display", "block");
+    } else {
+      $(".see-password").css("display", "none");
+    }
+  });
 });
